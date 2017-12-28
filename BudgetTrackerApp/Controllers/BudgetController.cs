3016aa152974309377c1ee26cf4f3bf065b7ce60
Daniel <daniel.tan@mohawkcollege.ca>
2017-12-27 23:22:56 -0500

    Added basic functionality to expenses page

--- BudgetTrackerApp/Controllers/BudgetController.cs
+++ BudgetTrackerApp/Controllers/BudgetController.cs
@@ -17,7 +17,13 @@ namespace BudgetTrackerApp.Controllers
         {
             ExpensesViewModel viewModel = new ExpensesViewModel();
             var budgetId = Convert.ToInt32(Request.Cookies["BudgetId"].Value);
-            viewModel.Categories = db.Categories.Where(c => c.BudgetId == budgetId).ToList();
+            viewModel.Categories = db.Categories.Where(c => c.BudgetId == budgetId)
+                .Select(c => new SelectListItem
+                    {
+                        Value = c.CategoryId.ToString(),
+                        Text = c.Name
+                    }).ToList();
+            viewModel.Expenses = db.Expenses.Where(e => e.BudgetId == budgetId).ToList();
             return View(viewModel);
         }
 
@@ -37,6 +43,72 @@ namespace BudgetTrackerApp.Controllers
         public ActionResult Calculator()
         {
             return View();
+        }
+
+        // POST: CreateExpense
+        [HttpPost]
+        public ActionResult CreateExpense(DateTime date, string categoryId, string description, float amount)
+        {
+            if (ModelState.IsValid)
+            {
+                Expense newExpense = new Expense();
+                newExpense.CategoryId = Convert.ToInt32(categoryId);
+                newExpense.BudgetId = Convert.ToInt32(Request.Cookies["BudgetId"].Value);
+                newExpense.Description = description;
+                newExpense.Date = date;
+                newExpense.Amount = (decimal)amount;
+                db.Expenses.Add(newExpense);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Expenses");
+        }
+
+        // POST: CreateCategory
+        [HttpPost]
+        public ActionResult CreateCategory(string categoryName)
+        {
+            if (ModelState.IsValid)
+            {
+                Category newCategory = new Category();
+                newCategory.Name = categoryName;
+                newCategory.BudgetId = Convert.ToInt32(Request.Cookies["BudgetId"].Value);
+                db.Categories.Add(newCategory);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Expenses");
+        }
+
+        // POST: EditCategory
+        [HttpPost]
+        public ActionResult EditCategory(string categoryId, string categoryName)
+        {
+            if (ModelState.IsValid && !string.IsNullOrEmpty(categoryId))
+            {
+                var budgetId = Convert.ToInt32(Request.Cookies["BudgetId"].Value);
+                var categoryIdConverted = Convert.ToInt32(categoryId);
+                Category editCategory = db.Categories.Single(c => c.BudgetId == budgetId && c.CategoryId == categoryIdConverted);
+                editCategory.Name = categoryName;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Expenses");
+        }
+
+        // POST: DeleteCategory
+        [HttpPost]
+        public ActionResult DeleteCategory(string categoryId)
+        {
+            if (ModelState.IsValid && !string.IsNullOrEmpty(categoryId))
+            {
+                var budgetId = Convert.ToInt32(Request.Cookies["BudgetId"].Value);
+                var categoryIdConverted = Convert.ToInt32(categoryId);
+                Category deleteCategory = db.Categories.FirstOrDefault(c => c.BudgetId == budgetId && c.CategoryId == categoryIdConverted);
+                if (deleteCategory != null)
+                {
+                    db.Categories.Remove(deleteCategory);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Expenses");
         }
 
         protected override void Dispose(bool disposing)
--- BudgetTrackerApp/Models/BudgetViewModels.cs
+++ BudgetTrackerApp/Models/BudgetViewModels.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BudgetTrackerApp.Models
 {
     public class ExpensesViewModel
     {
-        public List<Category> Categories { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public List<Expense> Expenses { get; set; }
     } 
 }
