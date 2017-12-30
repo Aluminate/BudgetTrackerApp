93826a7158b1599aa7032bf6eafa30426747f79e
Daniel <daniel.tan@mohawkcollege.ca>
2017-12-29 20:31:08 -0500

    Added feedback feature

--- BudgetTrackerApp/Controllers/HomeController.cs
+++ BudgetTrackerApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace BudgetTrackerApp.Controllers
 {
     public class HomeController : Controller
     {
+        private MyBudgetTrackerAppEntities db = new MyBudgetTrackerAppEntities();
+
         public ActionResult Index()
         {
             if (Request.IsAuthenticated)
@@ -23,6 +26,24 @@ namespace BudgetTrackerApp.Controllers
             return View();
         }
 
+        // POST: CreateFeedback
+        [HttpPost]
+        public ActionResult CreateFeedback(bool isPublic, string comment)
+        {
+            if (ModelState.IsValid)
+            {
+                Feedback feedback = new Feedback();
+                feedback.IsTestimonial = isPublic;
+                feedback.Message = comment;
+                feedback.IsHidden = false;
+                feedback.CreatedDate = DateTime.Now;
+                feedback.UserId = User.Identity.GetUserId();
+                db.Feedbacks.Add(feedback);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Dashboard");
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -35,6 +56,15 @@ namespace BudgetTrackerApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
